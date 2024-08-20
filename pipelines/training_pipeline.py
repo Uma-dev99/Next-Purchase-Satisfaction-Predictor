9987c0f4a6419285a16e1e591816df5e097b025e
uma-dev99 <umashakar.singh24101999@ug.cusat.ac.in>
2024-08-20 19:18:23 +0530

    Running Pipeline Success

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -1,6 +1,13 @@
-from zenml import pipelines
+from zenml import pipeline
 from steps.ingest_data import ingest_df
-from steps.cleaned_data import clean_df
-@pipelines
-def training_pipeline(data_path:str):
+from steps.clean_data import clean_df
+from steps.model_train import train_model
+from steps.evaluation import evaluate_model
+@pipeline
+def train_pipeline(data_path:str):
+    df=ingest_df(data_path)
+    clean_df(df)
+    train_model(df)
+    evaluate_model(df)
+
 
